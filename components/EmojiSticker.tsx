5f5f1a9efd40debd42532eee2fecd01423fc25b2
Adrian <ooikeanhong168@gmail.com>
2024-10-16 11:07:13 +0800

    Update: fixing animated image

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,6 +1,7 @@
-import { View } from 'react-native';
-import { Image } from 'expo-image';
-import React from 'react';
+import { View } from "react-native";
+import { Image } from "expo-image";
+import React from "react";
+import Animated from "react-native-reanimated";
 
 type Props = {
   imageSize: number;
@@ -10,7 +11,12 @@ type Props = {
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   return (
     <View style={{ top: -350 }}>
-      <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+      {/* <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} /> */}
+      {/* <Animated.Image
+        source={stickerSource}
+        resizeMode="contain"
+        style={{ width: imageSize, height: imageSize }}
+      /> */}
     </View>
   );
 }
